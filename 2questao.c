a581609085a82325522c75c5deda4fc8d8d360e1
bispopedroiago <pedroiagolimalimalima0203@gmail.com>
2025-07-23 08:50:15 -0300

    Update 2questao.c

--- 2questao.c
+++ 2questao.c
@@ -3,25 +3,25 @@
 
 int main(){
 	
-	int num1, num2, soma, sub, multi, divi, res;
+	int num1, num2, somaa, sube, multil, divi, resto;
 	
-	printf("Informe o primeiro numero inteiro: ");
+	printf("Informe o numero inteiro: ");
 	scanf("%d", &num1);
-	printf("Informe o segundo numero inteiro: ");
+	printf("Informe o numero inteiro: ");
 	scanf("%d", &num2);
 	
-	soma = num1 + num2;
-	sub = num1 - num2;
-	multi = num1 * num2;
+	somaa = num1 + num2;
+	sube = num1 - num2;
+	multil = num1 * num2;
 	divi = num1 / num2;
-	res = num1 % num2;
+	resto = num1 % num2;
 	
 	printf("Numero 1 \t Numero 2 \t Operacao \n");
-    printf("%d \t \t %d \t \t numero1 + numeo2 = %d \n", num1, num2, soma);
-    printf("%d \t \t %d \t \t numero1 - numeo2 = %d \n", num1, num2, sub);
-    printf("%d \t \t %d \t \t numero1 * numeo2 = %d \n", num1, num2, multi);
+    printf("%d \t \t %d \t \t numero1 + numeo2 = %d \n", num1, num2, somaa);
+    printf("%d \t \t %d \t \t numero1 - numeo2 = %d \n", num1, num2, sube);
+    printf("%d \t \t %d \t \t numero1 * numeo2 = %d \n", num1, num2, multil);
     printf("%d \t \t %d \t \t numero1 / numeo2 = %d \n", num1, num2, divi);
-    printf("%d \t \t %d \t \t numero1 %% numeo2 = %d \n", num1, num2, res
+    printf("%d \t \t %d \t \t numero1 %% numeo2 = %d \n", num1, num2, resto
     );
 
 
